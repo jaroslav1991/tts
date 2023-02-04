854f06435bec40f0b6cd4dab2727cc67824b1614
Andrey Tikhonov <andrey.tikhonov@exness.com>
2023-02-04 19:10:34 +0200

    todos

--- internal/service/dispatcher/interfaces.go
+++ internal/service/dispatcher/interfaces.go
@@ -2,10 +2,12 @@ package dispatcher
 
 import "github.com/jaroslav1991/tts/internal/model"
 
+// TODO implement http sender to remote server
 type Sender interface {
 	Send(data []model.DataModel) error
 }
 
+// TODO implement storage
 type Storage interface {
 	FixDataToSend() error
 	ClearSentData(file string) error

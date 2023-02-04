629c66191065ae684ae011ae42a1b1d454acd88a
jaroslav <jaroslav.shashunov@gmail.com>
2023-02-04 17:26:51 +0300

    Saver logic and test case

--- internal/service/data/saver.go
+++ internal/service/data/saver.go
@@ -1,14 +1,34 @@
 package data
 
-import "github.com/jaroslav1991/tts/internal/service"
+import (
+	"errors"
+	"fmt"
+	"github.com/jaroslav1991/tts/internal/service"
+	"os"
+)
+
+var (
+	ErrCantOpenFile      = errors.New("can't open file")
+	ErrCantWriteDataFile = errors.New("can't write data to file")
+)
 
 type Saver struct {
 	service.DataSaver
+	FileName string
 }
 
-// SaveData
-// todo implement SaveData logic
-// todo implement test for SaveData
-func (s *Saver) SaveData([]byte) error {
-	panic("implement me")
+func (s *Saver) SaveData(data []byte) error {
+	file, err := os.OpenFile(s.FileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrCantOpenFile, err)
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(string(data) + "\n")
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrCantWriteDataFile, err)
+	}
+
+	return nil
 }
---
+++ internal/service/data/saver_test.go
@@ -0,0 +1,31 @@
+package data
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestSaver_SaveData(t *testing.T) {
+	f, err := os.CreateTemp(os.TempDir(), "testfile")
+	assert.NoError(t, err)
+
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	saver := Saver{FileName: f.Name()}
+
+	actualErr := saver.SaveData([]byte(`test`))
+	assert.NoError(t, actualErr)
+
+	actualData, err := os.ReadFile(f.Name())
+	assert.NoError(t, err)
+	assert.Equal(t, "test\n", string(actualData))
+}
+
+func TestSaver_SaveData_InvalidFileName(t *testing.T) {
+	saver := Saver{}
+
+	actualErr := saver.SaveData([]byte(`test`))
+	assert.ErrorIs(t, actualErr, ErrCantOpenFile)
+}
